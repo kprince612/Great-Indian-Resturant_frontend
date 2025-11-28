090ef93fcd70585a4af17d820e8c2b25831375ee
kprince612 <khandelwalprince612@gmail.com>
2025-11-28 16:10:11 +0530

    Modify About Us section in About.js
    Updated paragraph content in About component.

--- src/Mycomponent/About.js
+++ src/Mycomponent/About.js
@@ -31,7 +31,8 @@ export default function About() {
             <h4>Our Story</h4>
             <h2>ABOUT US</h2>
             <p className='a'>. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde possimus natus deleniti! Eius distinctio natus aspernatur, harum quibusdam maiores, provident optio voluptatum illum sit sunt. Laboriosam porro ab, blanditiis ad odit a neque dolore iusto, quis ipsam labore numquam iste itaque? Cumque vel sequi libero quisquam, aspernatur alias.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde possimus natus deleniti! Eius distinctio natus aspernatur, harum quibusdam maiores, provident optio voluptatum illum sit sunt. Laboriosam porro ab, blanditiis ad odit a neque dolore iusto, quis ipsam labore numquam iste itaque? Cumque vel sequi libero quisquam, aspernatur alias.
+    </p>
             <br />
             <div className="a1">
             <a href="/Verify">Order Now
